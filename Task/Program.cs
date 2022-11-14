1ec5f7d3016a42521a77500ba9092920d84ac7ae
LWDima <volovichenko2012@yandex.ru>
2022-11-14 11:17:16 +0300

    created an algorithm

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите значения массива через пробел' ' ");
+string[] readArray = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string[] finalArray = new string[readArray.Length];
+SearchThreeChar(readArray, finalArray);
+PrintArray(finalArray);
+
+
+
+void SearchThreeChar(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
